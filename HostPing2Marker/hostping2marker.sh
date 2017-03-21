032cdf9297c3836928598267fe783a3d81fc0255
BooosesThaSnipper <BooosesThaSnipper@users.noreply.github.com>
2017-03-21 01:12:30 +0100

    first final script

--- HostPing2Marker/hostping2marker.sh
+++ HostPing2Marker/hostping2marker.sh
@@ -20,11 +20,15 @@
 # START - Declaration of Variables
 
 # Enter your LightManager Air IP
-LMA_IP='192.168.178.254'
+LMA_IP='xxx.xxx.xxx.xxx'
 
 # Enter the IP of the Host you want to check
-HOST_IP='192.168.178.253'
+HOST_IP='xxx.xxx.xxx.xxx'
 
+# Enter The Numer of Scene which will activate the Marker
+SCENE_ON="xx"
+# Enter The Numer of Scene which will activate the Marker
+SCENE_OFF="xx"
 
 # END - Declaration of Variables 
 # DO NOT CHANGE ANYTHING ABOVE THIS LINE
@@ -44,12 +48,23 @@ if [ $STATUS_FPING -ne 0 ]; then
 	exit 1
 fi
 
+# Check if curl is installed
+STATUS_CURL=$( dpkg -s curl &> /dev/null; echo $? )
+if [ $STATUS_CURL -ne 0 ]; then
+	echo "curl is not installed"
+	echo "Command for installing curl: \"sudo apt-get install curl\" "
+	exit 1
+fi
+
+
 # Check if Host is online
 STATUS_CHECK=$( fping -q ${HOST_IP}; echo $? )
 if [ ${STATUS_CHECK} -eq 0 ]; then
 	echo "Host Online"
+	curl http://${LMA_IP}/control?key=${SCENE_ON}
 elif [ ${STATUS_CHECK} -eq 1 ]; then
 	echo "Host Offline"
+	curl http://${LMA_IP}/control?key=${SCENE_OFF}
 else
 	echo "Unexpeted Error during Host Check"
 	exit 2
