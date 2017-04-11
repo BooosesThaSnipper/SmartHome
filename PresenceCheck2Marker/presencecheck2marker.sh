bc383c7b22894231af84950b3bf49ad4dcd53fda
BooosesThaSnipper <BooosesThaSnipper@users.noreply.github.com>
2017-04-11 21:37:49 +0200

    Optimize Smartphone detection and add nice output

--- PresenceCheck2Marker/presencecheck2marker.sh
+++ PresenceCheck2Marker/presencecheck2marker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # ########################################################################### #
 # =========================================================================== #
-# Filename:	presencecheck2marker.sh
-# Author:	BooosesThaSnipper
-# Version:	0.1
-# Date:		2017-04-09
-# Project:	SmartHome
+# Filename:     PresenceCheck2Marker.sh
+# Author:       BooosesThaSnipper
+# Version:      0.1
+# Date:         2017-03-20
+# Project:      SmartHome
 # =========================================================================== #
-# Description:	
+# Description:
 # A Script to check if Smartphones logged into the lokal WLAN to detect
 # presence at Home and depeinding from it, set LightManager Marker
 # =========================================================================== #
@@ -20,20 +20,17 @@
 # START - Declaration of Variables
 
 # Enter your LightManager Air IP
-LMA_IP='xxx.xxx.xxx.xxx'
+LMA_IP='192.168.178.10'
 
-# Enter the IP of the Smartphones you want to check
-## for one Smartphone 
-# SMARTPHONE_IP='xxx.xxx.xxx.xxx'
-## for two or more use space seperated list
-SMARTPHONE_IP='xxx.xxx.xxx.xxx xxx.xxx.xxx.xxx xxx.xxx.xxx.xxx'
+# Enter the IP of the Host you want to check
+SMARTPHONE_IP='192.168.178.20 192.168.178.21 192.168.178.90'
 
 # Enter The Numer of Scene which will activate the Marker
-SCENE_ON="xx"
+SCENE_ON="100"
 # Enter The Numer of Scene which will activate the Marker
-SCENE_OFF="xx"
+SCENE_OFF="101"
 
-# END - Declaration of Variables 
+# END - Declaration of Variables
 # DO NOT CHANGE ANYTHING ABOVE THIS LINE
 # =========================================================================== #
 # ########################################################################### #
@@ -46,42 +43,61 @@ SCENE_OFF="xx"
 # Check if netcat is installed
 STATUS_NMAP=$( dpkg -s nmap &> /dev/null; echo $? )
 if [ $STATUS_NMAP -ne 0 ]; then
-	echo "nmap is not installed"
-	echo "Command for installing nmap: \"sudo apt-get install nmap\" "
-	exit 1
+        echo "nmap is not installed"
+        echo "Command for installing nmap: \"sudo apt-get install nmap\" "
+        exit 1
 fi
 
 # Check if curl is installed
 STATUS_CURL=$( dpkg -s curl &> /dev/null; echo $? )
 if [ $STATUS_CURL -ne 0 ]; then
-	echo "curl is not installed"
-	echo "Command for installing curl: \"sudo apt-get install curl\" "
-	exit 1
+        echo "curl is not installed"
+        echo "Command for installing curl: \"sudo apt-get install curl\" "
+        exit 1
 fi
 
+DATE=$( date +%F-%N )
+echo "${DATE} - Start Presence Check"
 
 # Check if Smartphonees logged in"
 PRESENCE=0
 for SMARTPHONE in ${SMARTPHONE_IP}; do
-	if [ $(nmap -sP ${SMARTPHONE} | grep -q "Host seems down"; echo $? )  -eq 0 ]; then
-		echo $SMARTPHONE offline
-	elif [ $(nmap -sP ${SMARTPHONE} | grep -q "Host is up" ; echo $? ) -eq 0 ]; then
-		echo $SMARTPHONE online
-		PRESENCE=1
-        else   
+        DATE=$( date +%F-%N )
+        echo "${DATE} - ${SMARTPHONE} check running"
+        sudo nmap -sU -sT ${SMARTPHONE} -p U:5353,T:62078 > /dev/null
+        sleep 1
+        if [ $( sudo nmap -sU -sT ${SMARTPHONE} -p U:5353,T:62078 | grep -q "Host seems down"; echo $? )  -eq 0 ]; then
+                DATE=$( date +%F-%N )
+                echo "${DATE} - $SMARTPHONE offline"
+        elif [ $( sudo nmap -sU -sT ${SMARTPHONE} -p U:5353,T:62078 | grep -q "Host is up" ; echo $? ) -eq 0 ]; then
+                DATE=$( date +%F-%N )
+                echo "${DATE} - $SMARTPHONE online"
+                PRESENCE=1
+        else
                 echo "Unexpeted Error during Host Check"
-	fi
+        fi
+
+        DATE=$( date +%F-%N )
+        echo "${DATE} - -----"
 done
 
 # Set Marker Depending from Presence Status
 if [ ${PRESENCE} -eq 1 ]; then
-	echo "Presence activ"
-	curl http://${LMA_IP}/control?key=${SCENE_ON}
-else 
-	echo "Presence deactive"
-	curl http://${LMA_IP}/control?key=${SCENE_OFF}
+        DATE=$( date +%F-%N )
+        echo "${DATE} - Presence activ"
+        STATUS=$( curl -s http://${LMA_IP}/control?key=${SCENE_ON} )
+        DATE=$( date +%F-%N )
+        echo "${DATE} - LightManager Marker Update Status: ${STATUS}"
+else
+        DATE=$( date +%F-%N )
+        echo "${DATE} - Presence deactive"
+        STATUS=$( curl -s http://${LMA_IP}/control?key=${SCENE_OFF} )
+        DATE=$( date +%F-%N )
+        echo "${DATE} - LightManager Marker Update Status: ${STATUS}"
 fi
 
+DATE=$( date +%F-%N )
+echo "${DATE} - -------------------------------------"
 
 # =========================================================================== #
 # ########################################################################### #
